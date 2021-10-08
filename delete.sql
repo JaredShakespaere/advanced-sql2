2f799520673beb78cf60e0d0cc219b44f922d643
Jared Shakespaere <jaredshakespaere@Jareds-MacBook-Pro.local>
2021-10-07 21:48:02 -0500

    final-deletesql

---
+++ delete.sql
@@ -0,0 +1,18 @@
+-- Q.2
+DELETE 
+FROM animals
+WHERE type = 'lion';
+-- Q.3
+DELETE
+FROM animals
+WHERE name LIKE 'M%';
+
+SELECT *
+FROM animals
+-- Q.4
+DELETE
+FROM animals
+WHERE age < 9;
+
+SELECT * 
+FROM animals
